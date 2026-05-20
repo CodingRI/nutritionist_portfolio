cf400c8e089d3fb9b05a555c00c6ea7806da156c
CodingRI <125729609+CodingRI@users.noreply.github.com>
2026-05-21 01:35:12 +0530

    fix: middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -135,7 +135,11 @@ export default clerkMiddleware(
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*).*)",
-    "/(api|trpc)(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    // Always run for Clerk-specific frontend API routes
+    '/__clerk/(.*)',
   ],
 };
